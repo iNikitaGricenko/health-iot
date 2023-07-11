7aadde7c1d21df7cf98a59ece5599464cecfa8ab
iNikitaGricenko <VfVristalt@gmail.com>
2023-07-11 17:55:52 +0300

    Create fauna repository and exception handler

--- src/main/java/com/inikitagricenko/healthy/service/implement/HealthService.java
+++ src/main/java/com/inikitagricenko/healthy/service/implement/HealthService.java
@@ -1,12 +1,10 @@
 package com.inikitagricenko.healthy.service.implement;
 
-import com.faunadb.client.FaunaClient;
-import com.faunadb.client.query.Expr;
-import com.faunadb.client.types.Value;
 import com.inikitagricenko.healthy.annotation.AOPLog;
+import com.inikitagricenko.healthy.exception.DataNotFound;
 import com.inikitagricenko.healthy.model.Coordinates;
 import com.inikitagricenko.healthy.model.HealthData;
-import com.inikitagricenko.healthy.service.IFaunaService;
+import com.inikitagricenko.healthy.repository.HealthRepository;
 import com.inikitagricenko.healthy.service.IGeoLocationService;
 import com.inikitagricenko.healthy.service.IHealthOutputService;
 import com.inikitagricenko.healthy.service.IHealthProcessService;
@@ -14,19 +12,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.net.MalformedURLException;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
-
-import static com.faunadb.client.query.Language.*;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class HealthService implements IHealthProcessService, IHealthOutputService {
 
 	private final IGeoLocationService geoLocationService;
-	private final IFaunaService faunaService;
+	private final HealthRepository healthRepository;
 
 	@AOPLog
 	@Override
@@ -34,64 +26,13 @@ public class HealthService implements IHealthProcessService, IHealthOutputServic
 		Coordinates coordinates = healthData.getCoordinates();
 		coordinates = geoLocationService.getCoordinates(coordinates.getLatitude(), coordinates.getLongitude());
 
-		store(healthData, coordinates);
+		healthRepository.save(healthData);
 	}
 
 	@AOPLog
 	@Override
 	public HealthData responseHealth(String userId, Coordinates coordinates) {
-		coordinates = geoLocationService.getCoordinates(coordinates.getLatitude(), coordinates.getLongitude());
-
-		try {
-			FaunaClient faunaClient = faunaService.getFaunaClient(coordinates.getCountryCode());
-
-			Expr query = Get(Ref(Collection("healthdata"), userId));
-
-			Value result = faunaClient.query(
-					Let("data", query).in(
-							Obj("data", Var("data"),
-									"userId", Get(
-											Select(
-													Arr(
-															Value("temperature"),
-															Value("pulseRate"),
-															Value("bpSystolic"),
-															Value("bpDiastolic"),
-															Value("latitude"),
-															Value("longitude"),
-															Value("country_code"),
-															Value("time_zone_id")),
-													Var("data")))))).get();
-			return result.get(HealthData.class);
-		} catch (MalformedURLException | ExecutionException | InterruptedException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	@AOPLog
-	private void store(HealthData healthData, Coordinates coordinates) {
-		try {
-			FaunaClient faunaClient = faunaService.getFaunaClient(coordinates.getCountryCode());
-
-			Value queryResponse = faunaClient.query(
-					Create(
-							Collection("healthdata"), Obj("data",
-									Obj(
-											Map.of(
-													"userId", Value(healthData.getUserId()),
-													"temperature", Value(healthData.getTemperature()),
-													"pulseRate", Value(healthData.getPulseRate()),
-													"bpSystolic", Value(healthData.getBpSystolic()),
-													"bpDiastolic", Value(healthData.getBpDiastolic()),
-													"latitude", Value(coordinates.getLatitude()),
-													"longitude", Value(coordinates.getLongitude()),
-													"country_code", Value(coordinates.getCountryCode()),
-													"time_zone_id", Value(coordinates.getTimezoneId()),
-													"timestamp", Now())))
-					      )).get();
-			log.info("Query response received from Fauna: {}", queryResponse);
-		} catch (MalformedURLException | ExecutionException | InterruptedException e) {
-			throw new RuntimeException(e);
-		}
+		String countryCode = geoLocationService.getCoordinates(coordinates.getLatitude(), coordinates.getLongitude()).getCountryCode();
+		return healthRepository.findById(userId, countryCode).orElseThrow(DataNotFound::new);
 	}
 }
